38827bdda3e32d49bc36fc986dc762ad5d372314
Thomas Brunström <tbn@du.se>
2020-02-04 11:44:49 +0100

    Initial commit

---
+++ rickandmorty.js
@@ -0,0 +1,10 @@
+const request = require('request'); //Inkludera request, npm install request --save
+request.get('https://rickandmortyapi.com/api/character/?name=pickle rick', (e, a) => {
+    if(!e) {
+        data = JSON.parse(a.body); //Gör om till json
+        ricks = data.results; //Använd resultatet
+        ricks.forEach((r) => { //Loopa igenom alla resultat
+            console.log(r.image);
+        });
+    }
+});
---
+++ userinput.js
@@ -0,0 +1,13 @@
+const standardInput = process.stdin;
+standardInput.setEncoding('utf-8');
+console.log('Hej, vad heter du?');
+standardInput.on('data', (data) => {
+    if(data.trim() === 'exit') {
+        console.log('Tack för mig, vi ses.');
+        process.exit();
+    }
+    else {
+        console.log(`Hej ${data.replace('\n', '')}, 
+            trevligt att träffas`);
+    }
+});
