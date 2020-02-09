a2953ec71fb6f104cee1c81309691623a7cb2ac1
Thomas Brunström <choice@warzone.se>
2020-02-09 19:21:00 +0100

    Update examples

--- database.js
+++ database.js
@@ -9,18 +9,26 @@ const doQueryCB = () => {
                 console.log(rows);
             })
             .catch((error) => {
-                console.log(e)
+                console.log('Något gick fel');
+                console.log(error)
             })
             .finally(() => {
                 console.log('Were done');
             });
     });
-}
+};
 
 const doQuery = async () => {
-    const db = await dbCon;
-    const users = await db.all('SELECT username, id FROM users ORDER BY username ASC');
-    console.log(users);
+    try {
+        const db = await dbCon;
+        const users = await db.all('SELECT username, id FROM users ORDER BY username ASC');
+        console.log(users);
+    }
+    catch(error) {
+        console.log('Något gick fel.');
+        console.log(error);
+
+    }
 };
 doQueryCB();
 doQuery();
